745f3949978069c24bb6abbe4b310d94d1e93491
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-16 21:14:05 +0100

    update

--- src/components/VideoList/VideoList.styled.js
+++ src/components/VideoList/VideoList.styled.js
@@ -1,12 +1,17 @@
 import styled from '@emotion/styled';
 export const Ul = styled.ul`
 border:1px solid purple;
+background-color: #f484f4;
 padding:10px
 `;
 
 export const Li = styled.li`
 list-style: none;
+padding: 10px;
 border: 1px solid purple;
 margin-bottom: 10px;
-border-radius: 2px 3px 4px 
+border-radius: 5px;
+box-shadow: 6px 7px 8px rgb(85, 42, 85);
+background-color: white;
+cursor: pointer
 `;
